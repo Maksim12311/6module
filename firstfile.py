f7f3cc8588a9ec8a23966a79c8daf6b1e48d2d46
Maksim12311 <max.brus.2016@gmail.com>
2024-01-23 15:41:25 +0300

    It s first task

--- firstfile.py
+++ firstfile.py
@@ -1,1 +1,15 @@
-print("Solutions for module 6");
+import time
+from threading import Thread
+
+def get_thread(thread_name):
+	time.sleep(1)
+	print(f'The flow {thread_name} started!')
+
+list = ['a','b','c','d','e']
+threads = [Thread(target = get_thread,args = (i,)) for i in list]
+
+for t in threads:
+	t.start()
+
+for t in threads:
+	t.join()
